0ced32c5010483c8d40331e8cd395797530cd105
GonzaloSantiago <gsantiago_o@hotmail.com>
2025-10-16 12:44:30 -0600

    probando getsolicitudes

--- js/listSolicitudes.js
+++ js/listSolicitudes.js
@@ -34,8 +34,8 @@ export default async function listSolicitudes(data) {
       $clone
         .querySelector("[data-td-id]")
         .setAttribute("data-td-id", el.solicitud_id);
-      $clone.querySelector("[data-name]").textContent = el.usuario_nombre;
-      $clone.querySelector("[data-name]").textContent = el.usuario_nombre;
+      $clone.querySelector("[data-name]").textContent =
+        el.usuario_nombre_completo;
       $clone.querySelector("[data-mail]").textContent = el.usuario_correo;
       $clone.querySelector("[data-depto]").textContent =
         el.usuario_departamento_nombre;
