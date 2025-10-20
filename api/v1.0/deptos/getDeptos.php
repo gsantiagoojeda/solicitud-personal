729bdcbd3d571048d0eda79efcd572c2ad3627f8
GonzaloSantiago <gsantiago_o@hotmail.com>
2025-10-20 10:09:33 -0600

    quitando fondo gris auth

--- api/v1.0/deptos/getDeptos.php
+++ api/v1.0/deptos/getDeptos.php
@@ -1,7 +1,7 @@
 <?php
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require "../conexion_intranet.php";
 require "../conexion_vacaciones.php";
 
