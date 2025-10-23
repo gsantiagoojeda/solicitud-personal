fb3ffb5a59f55e4bf6174ccda531aa7d6145f20d
GonzaloSantiago <gsantiago_o@hotmail.com>
2025-10-23 10:05:07 -0600

    agregando getSolcitud

--- api/v1.0/solicitudes/getSolicitud.php
+++ api/v1.0/solicitudes/getSolicitud.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require "../conexion_intranet.php";
 require "../conexion_vacaciones.php";
 require "../conexion_solicitud.php";
