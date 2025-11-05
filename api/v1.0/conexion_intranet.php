d727337d7a0b76e2e42672f333d113e390308b0e
GonzaloSantiago <gsantiago_o@hotmail.com>
2025-11-05 10:17:51 -0600

    arreglando getsol

--- api/v1.0/conexion_intranet.php
+++ api/v1.0/conexion_intranet.php
@@ -7,5 +7,8 @@ $dbname = "intranet_alze";  // Nombre de la base de datos
 
 $mysqli_intranet = new mysqli($servername, $username, $password, $dbname);
 
+if (!$mysqli_intranet->set_charset("utf8mb4")) {
+    die("Error al configurar el charset UTF-8: " . $mysqli_intranet->error);
+}
 
 ?>
