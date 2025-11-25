3eee6432448e1e80387102ee1b8bea2f50646338
GonzaloSantiago <gsantiago_o@hotmail.com>
2025-11-25 10:10:13 -0600

    probando filtros

--- js/validateRangeFilter.js
+++ js/validateRangeFilter.js
@@ -1,14 +1,24 @@
 const d = document;
 
 export default function validateRangeFilter(e) {
-  console.log("rango");
   if (
-    !e.target.matches("#start-year") &&
-    !e.target.matches("#start-year *") &&
-    !e.target.matches("#end-year") &&
-    !e.target.matches("#end-year *")
-  )
+    !location.pathname.includes("ver-solicitudes.html") ||
+    (!e.target.matches("#start-year") &&
+      !e.target.matches("#start-year *") &&
+      !e.target.matches("#end-year") &&
+      !e.target.matches("#end-year *"))
+  ) {
     return;
+  }
 
-  console.log(e.target);
+  const $startYear = d.getElementById("start-year");
+  let startYear = $startYear.value;
+  const $endYear = d.getElementById("end-year");
+  let endYear = $endYear.value;
+
+  if (parseInt(startYear) < parseInt(endYear)) {
+    $endYear.classList.remove("border-gray-600");
+    $endYear.classList.add("text-red-600");
+    $endYear.classList.add("border-red-600");
+  }
 }
