2abbf5dae341bf565f9d698cc89ca2ca416916ea
GonzaloSantiago <gsantiago_o@hotmail.com>
2025-11-27 12:03:57 -0600

    listado mis solicitudes

--- js/listMisSolicitudes.js
+++ js/listMisSolicitudes.js
@@ -34,43 +34,61 @@ export default async function listMisSolicitudes(data) {
       $clone
         .querySelector("[data-td-id]")
         .setAttribute("data-td-id", el.solicitud_id);
-      $clone.querySelector("[data-name]").textContent =
-        el.usuario_nombre_completo;
-      $clone.querySelector("[data-empresa]").textContent = el.usuario_empresa;
-      $clone.querySelector("[data-depto]").textContent =
-        el.usuario_departamento_nombre;
-      $clone.querySelector("[data-puesto]").textContent =
-        el.usuario_puesto.length > 40
-          ? el.usuario_puesto.substring(0, 40) + "..."
-          : el.usuario_puesto;
       $clone.querySelector("[data-vacante]").textContent =
         el.solicitud_nombre_puesto;
-      const statusElement = $clone.querySelector("[data-status]");
+      const status1Element = $clone.querySelector("[data-status1]");
+      const status2Element = $clone.querySelector("[data-status2]");
 
       if (el.solicitud_autorizacion1 === "") {
-        statusElement.textContent = "Pendiente";
-        statusElement.classList.add("bg-green-600");
+        status1Element.textContent = "Pendiente";
+        status1Element.classList.add("bg-green-600");
       } else if (el.solicitud_autorizacion1 === "Rechazada") {
-        statusElement.textContent = el.solicitud_autorizacion1;
-        statusElement.classList.add("bg-red-600");
+        status1Element.textContent = el.solicitud_autorizacion1;
+        status1Element.classList.add("bg-red-600");
       } else {
-        statusElement.textContent = "Autorizada";
-        statusElement.classList.add("bg-green-600");
+        status1Element.textContent = "Autorizada";
+        status1Element.classList.add("bg-green-600");
       }
-      $clone.querySelector("[data-auth]").textContent =
+      $clone.querySelector("[data-auth1]").textContent =
         el.autorizador1_nombre_completo === ""
           ? "-"
           : el.autorizador1_nombre_completo;
-      $clone.querySelector("[data-authpuesto]").textContent =
+      $clone.querySelector("[data-auth1puesto]").textContent =
         el.autorizador1_puesto === ""
           ? "-"
           : el.autorizador1_puesto.length > 40
           ? el.autorizador1_puesto.substring(0, 40) + "..." // Trunca y añade "..."
           : el.autorizador1_puesto;
-      $clone.querySelector("[data-authdate]").textContent =
+      $clone.querySelector("[data-auth1date]").textContent =
         el.solicitud_date_autorizacion1 === ""
           ? "-"
           : el.solicitud_date_autorizacion1;
+
+      //auth2
+      if (el.solicitud_autorizacion2 === "") {
+        status2Element.textContent = "Pendiente";
+        status2Element.classList.add("bg-green-600");
+      } else if (el.solicitud_autorizacion1 === "Rechazada") {
+        status2Element.textContent = el.solicitud_autorizacion1;
+        status2Element.classList.add("bg-red-600");
+      } else {
+        status2Element.textContent = "Autorizada";
+        status2Element.classList.add("bg-green-600");
+      }
+      $clone.querySelector("[data-auth2]").textContent =
+        el.autorizador2_nombre_completo === ""
+          ? "-"
+          : el.autorizador2_nombre_completo;
+      $clone.querySelector("[data-auth2puesto]").textContent =
+        el.autorizador2_puesto === ""
+          ? "-"
+          : el.autorizador2_puesto.length > 40
+          ? el.autorizador2_puesto.substring(0, 40) + "..." // Trunca y añade "..."
+          : el.autorizador2_puesto;
+      $clone.querySelector("[data-auth2date]").textContent =
+        el.solicitud_date_autorizacion2 === ""
+          ? "-"
+          : el.solicitud_date_autorizacion2;
 
       $fragment.appendChild($clone);
     });
