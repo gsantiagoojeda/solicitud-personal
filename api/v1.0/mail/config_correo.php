3bd96310106b071efda37adc0113142f6ee9fb16
GonzaloSantiago <gsantiago_o@hotmail.com>
2025-12-22 12:12:09 -0600

    mail

--- api/v1.0/mail/config_correo.php
+++ api/v1.0/mail/config_correo.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/../../vacaciones/vendor/autoload.php';
+require __DIR__ . '/../../../../vacaciones/vendor/autoload.php';
 
 function getMailer() {
     $mail = new PHPMailer(true);
