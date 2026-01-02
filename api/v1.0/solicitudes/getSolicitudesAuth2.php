2cecfd83797f7bcfbac8f7150105506a0aa756a1
GonzaloSantiago <gsantiago_o@hotmail.com>
2026-01-02 12:41:34 -0600

    poniendo filtros

--- api/v1.0/solicitudes/getSolicitudesAuth2.php
+++ api/v1.0/solicitudes/getSolicitudesAuth2.php
@@ -9,7 +9,7 @@ require "../conexion_solicitud.php";
 
 
 // $idUser = $_POST['user-id']; // usuario actual
-$idUser = "391"; // usuario actual
+$idUser = "3190"; // usuario actual
 
 $filterYearStart = $_POST['filterYearStart'] ?? null; 
 $filterYearEnd = $_POST['filterYearEnd'] ?? null;
